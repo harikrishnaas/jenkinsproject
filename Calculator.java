f8a9ca095101630105b6a28dabee39d2c60d184e
harikrishnaas <114973490+harikrishnaas@users.noreply.github.com>
2023-08-03 14:40:19 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+package com.toyota.junit;
+
+ 
+
+ 
+
+public class Calculator {
+
+    public int add(int i,int j) {
+
+        return i+j;
+
+    }
+
+    public int sub(int i,int j) {
+
+        return i-j;
+
+    }
+
+}
+
+ 
